d9542f1bac907b92c00455eab1e44f750a1d507e
Ivan <feleir@gmail.com>
2014-06-19 23:20:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-ajax({ url: 'http://simplyjs.io' }, function(data){
+ajax({ url: 'http://www.dublinbus.ie/en/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery=411' }, function(data){
   var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-  simply.title(headline);
+  simply.title(data);
 });
